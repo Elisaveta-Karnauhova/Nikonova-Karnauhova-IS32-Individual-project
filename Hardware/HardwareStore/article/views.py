9f09b26104057ee31ff291d133fbd4b93076bbb2
Your Name <you@example.com>
2025-01-15 12:45:05 +0300

    15.01.25 создание проекта до стр 17

--- Hardware/HardwareStore/article/views.py
+++ Hardware/HardwareStore/article/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Article
+from .serializers import ArticleSerializer
 
-# Create your views here.
+
+class ArticleView(APIView):
+ 
+ def get(self, request):
+    articles = Article.objects.all()
+    serializer = ArticleSerializer(articles, many=True)
+    return Response({"articles": serializer.data})
+ def post(self, request):
+    article = request.data.get('articles')
+    # Create an article from the above data
+    serializer = ArticleSerializer(data=article)
+    if serializer.is_valid(raise_exception=True):
+     article_saved = serializer.save()
+    return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+
